a792801a755d8c26f9f5cee4a76839afd7cd4d25
wadfe <wadfe@LIKUURPC>
2024-11-04 09:26:08 +0300

    Добавьте файлы проекта.

---
+++ Likuur`s Minecraft Launcher/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Likuur_s_Minecraft_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.verionlist = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nickname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maxram = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxram)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(56, -17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(454, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(209, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Версия: 1.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Выберите версию:";
+            // 
+            // verionlist
+            // 
+            this.verionlist.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.verionlist.FormattingEnabled = true;
+            this.verionlist.Location = new System.Drawing.Point(236, 119);
+            this.verionlist.Name = "verionlist";
+            this.verionlist.Size = new System.Drawing.Size(151, 21);
+            this.verionlist.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Введите ник игрока:";
+            // 
+            // nickname
+            // 
+            this.nickname.Location = new System.Drawing.Point(258, 148);
+            this.nickname.Name = "nickname";
+            this.nickname.Size = new System.Drawing.Size(129, 20);
+            this.nickname.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(184, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Войти в игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(387, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Выделите оперативную память:";
+            // 
+            // maxram
+            // 
+            this.maxram.Location = new System.Drawing.Point(396, 177);
+            this.maxram.Maximum = new decimal(new int[] {
+            64000,
+            0,
+            0,
+            0});
+            this.maxram.Minimum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.maxram.Name = "maxram";
+            this.maxram.Size = new System.Drawing.Size(120, 20);
+            this.maxram.TabIndex = 8;
+            this.maxram.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(521, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "МБ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 255);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.maxram);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nickname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.verionlist);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Likuur`s Minecraft Launcher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxram)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox verionlist;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nickname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown maxram;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Likuur`s Minecraft Launcher/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using CmlLib.Core;
+using CmlLib.Core.ProcessBuilder;
+using System.Windows.Forms;
+using CmlLib.Core.Auth;
+
+namespace Likuur_s_Minecraft_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadMCVersionsAcync();
+        }
+        private async void LoadMCVersionsAcync()
+        {
+            var launcher = new MinecraftLauncher();
+            var versions = await launcher.GetAllVersionsAsync();
+            foreach ( var version in versions )
+            {
+                verionlist.Items.Add(version.Name);
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string username = nickname.Text;
+            string version = string.Empty;
+            string mcpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "LMCLauncher");
+            Directory.CreateDirectory(mcpath);
+            var path = new MinecraftPath(mcpath);
+            var launcher = new MinecraftLauncher(path);
+
+            if (verionlist.SelectedIndex.ToString() != null && !string.IsNullOrEmpty(verionlist.SelectedIndex.ToString()))
+            {
+                version = verionlist.SelectedItem.ToString();
+                await launcher.InstallAsync(version);
+            }
+            else
+            {
+                MessageBox.Show("Выберите версию!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            var minecraftProcess = await launcher.BuildProcessAsync(version, new MLaunchOption
+            {
+                Session = MSession.CreateOfflineSession(username),
+                MaximumRamMb = (int)maxram.Value,
+                GameLauncherName = "Likuur`s Minecraft Launcher",
+                ServerIp = "mc.vimemc.net"
+            });
+
+            minecraftProcess.Start();
+        }
+    }
+}
